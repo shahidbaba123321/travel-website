ecf3e28637480ba80f15af1bffb5626eca240599
shahidbaba123321 <shahid.craftorbis@gmail.com>
2025-02-02 14:58:57 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,23 @@
+// src/components/Navbar.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">TravelWorld</div>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/tour-packages">Tour Packages</Link></li>
+        <li><Link to="/destinations">Destinations</Link></li>
+        <li><Link to="/cab-services">Cab Services</Link></li>
+        <li><Link to="/self-drive">Self Drive</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/gallery">Gallery</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
